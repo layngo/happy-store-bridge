f22bf6d6e3c7a1baa95253fbe25f222d15be9e01
AdamLaynGo <adam@layngo.com>
2026-06-09 08:22:23 -0400

    A-Images V3

--- src/pages/static/AboutUsV3.tsx
+++ src/pages/static/AboutUsV3.tsx
@@ -113,8 +113,6 @@ const CHAPTERS: StoryChapter[] = [
         src: aboutUsV2Png("cosmo-launch-2013.png"),
         title: "The COSMO launches",
         alt: "Models lying in a circle around an open Lay-n-Go COSMO cosmetic bag",
-        imagePosition: "center 42%",
-        imageScale: 1.12,
         previewText: "The success of the Lay-n-Go design was a perfect fit for cosmetics and makeup...",
         storyText:
           "The success of the Lay-n-Go design was a perfect fit for cosmetics and makeup. The COSMO was an instant hit and was launched at CosmoProf in Las Vegas. Soon the product could be found in Target, Costco, on QVC, and in thousands of independent retailers. The COSMO is the best selling product Lay-n-Go has ever launched.",
@@ -123,7 +121,6 @@ const CHAPTERS: StoryChapter[] = [
         src: aboutUsV2Png("inc-5000-block.png"),
         title: "Lay-n-Go named to the Inc 5000",
         alt: "Inc. 5000 America's Fastest-Growing Private Companies badge",
-        imagePosition: "center",
         previewText: "As the company grew, some exciting accolades came our way...",
         storyText:
           "Starting a business is one thing. Growing a business is a much harder challenge. Doing this with your spouse adds a whole other complexity. That being said, we have loved our journey, our incredible customers, and the team around us. YOU have made this dream happen!",
